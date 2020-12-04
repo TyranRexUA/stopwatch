a13425c4e66f78fbfa10ddfbd4deb63430078d19
TyranRexUA <tyranrexua@gmail.com>
2020-12-04 20:03:36 +0200

    create

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import { fromEvent, interval, Observable } from 'rxjs';
+import { map, buffer, debounceTime, filter } from 'rxjs/operators';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let observable$: Observable<number> = interval(1000)
+let subscriber: any;
+
+const App: React.FC = () => {
+    let [time, setTime] = useState<number>(0)
+    let [isStart, setIsStart] = useState<boolean>(false)
+    let ref = useRef(null)
+
+    useEffect(() => {
+
+        const click$: Observable<any> = fromEvent(ref.current as any, 'click')
+
+        const doubleClick$ = click$
+            .pipe(
+                buffer(click$.pipe(debounceTime(300))),
+                map(list => list.length),
+                filter(x => x === 2),
+            )
+
+        const DoubleClickSubscriber$ = doubleClick$.subscribe(() => {
+            setIsStart(false)
+            subscriber.unsubscribe()
+        })
+
+        return () => {
+            DoubleClickSubscriber$.unsubscribe()
+        }
+    }, [])
+
+    const start = () => {
+        setIsStart(true)
+        subscriber = observable$.subscribe(() => {
+            setTime(prevTime => ++prevTime)
+        })
+    }
+
+    const stop = () => {
+        setIsStart(false)
+        setTime(0)
+        subscriber.unsubscribe()
+    }
+
+    const reset = () => {
+        stop()
+        start()
+    }
+
+    return (
+        <div className='App'>
+            {Math.floor(time / 3600) + ":" + Math.floor(time / 60) + ":" + (time % 60)}
+
+            <div className='btngroup'>
+
+                <button onClick={isStart ? stop : start}>
+                    {isStart ? 'Stop' : 'Start'}
+                </button>
+
+                <button ref={ref}>
+                    Wait (doubleClick)
+                </button>
+
+                <button onClick={reset}>
+                    Reset
+                </button>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
